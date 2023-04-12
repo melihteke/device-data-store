6250aae84424ce612e146e1d15c86699108a938c
melihteke <36086368+melihteke@users.noreply.github.com>
2023-04-12 20:02:19 +0200

    Create environment.py

---
+++ environment.py
@@ -0,0 +1,11 @@
+
+import os
+import sys
+import pathlib
+from dotenv import load_dotenv
+
+
+dotenv_current_path = os.path.join(pathlib.Path().resolve(), '.env')
+load_dotenv(dotenv_current_path)
+
+DBA_URI = os.environ.get("DBA_URI")
